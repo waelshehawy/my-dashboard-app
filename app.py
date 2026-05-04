b4f243fd95d7b54123e13f2128d7ad56f8432822
waelshehawy <92762484+waelshehawy@users.noreply.github.com>
2026-05-04 10:38:15 +0300

    Update app.py

--- app.py
+++ app.py
@@ -149,7 +149,7 @@ def export_word(customer_name, cart_data, start_p, end_p):
                 
                 run = p_sum.add_run(txt)
                 run.bold = True
-                run.font.color.rgb = RGBColor(102, 0, 153) # لون موف للنص الإجمالي
+                run.font.color.rgb = RGBColor(0, 0, 0) # لون موف للنص الإجمالي
                 apply_rtl(p_sum)
                     # --- إضافة ملاحظة الصلاحية في نهاية المستند ---
                 doc.add_paragraph() # سطر فارغ للتباعد
@@ -159,7 +159,7 @@ def export_word(customer_name, cart_data, start_p, end_p):
                 run_note = p_note.add_run("• ملاحظة: هذه المواقع المتاحة سارية حتى 48 ساعة من تاريخ إرسالها للشركة.")
                 run_note.bold = True
                 run_note.font.size = Pt(12)
-                run_note.font.color.rgb = RGBColor(200, 0, 0) # لون أحمر هادئ للتنبيه
+                run_note.font.color.rgb = RGBColor(0, 0, 0) # لون أحمر هادئ للتنبيه
     
                 apply_rtl(p_note) # تطبيق اتجاه النص العربي
     
