cc61466232461c0fee96055b1ab13045998d9124
waelshehawy <92762484+waelshehawy@users.noreply.github.com>
2026-05-11 14:50:18 +0300

    Update app.py

--- app.py
+++ app.py
@@ -596,7 +596,7 @@ else:
                     for net in networks:
                             net_data = available_columns[available_columns['الشبكة'] == net].copy()
                             net_data['fee_print'] = fee_print
-                            net_data['fee_ads_monthly'] = fee_ads_monthly
+                            net_data['fee_ads'] = fee_ads_monthly
                         st.session_state.cart[selected_city][net] = net_data
                     
                     st.success(f"تمت الإضافة")
