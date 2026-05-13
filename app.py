7e73521ce21c143b1ac7d40a6f66e6794db0bd85
waelshehawy <92762484+waelshehawy@users.noreply.github.com>
2026-05-13 09:59:06 +0300

    Update app.py

--- app.py
+++ app.py
@@ -487,8 +487,10 @@ else:
                                 for city, networks in cart_raw.items():
                                     st.session_state.cart[city] = {}                            
 
-                    except Exception as e:
+                     except Exception as e:
                         st.error(f"خطأ في تحميل العرض: {str(e)}")
+                        import traceback
+                        st.code(traceback.format_exc())
             
             st.divider()
             
