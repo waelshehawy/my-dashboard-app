2bd6e2fae12ccd2c9b7e340566f427a5c468498e
waelshehawy <92762484+waelshehawy@users.noreply.github.com>
2026-06-18 14:02:52 +0300

    Update database.py

--- utils/database.py
+++ utils/database.py
@@ -1,22 +1,18 @@
 # utils/database.py
 import psycopg2
 import pandas as pd
-from sqlalchemy import create_engine
-from sqlalchemy.engine import URL
 
 def get_connection():
     """اتصال مباشر بـ Supabase PostgreSQL"""
     return psycopg2.connect(
-        host=os.environ.get("SUPABASE_HOST", "aws-1-eu-north-1.pooler.supabase.com"),
-        port=os.environ.get("SUPABASE_PORT", "6543"),
-        database=os.environ.get("SUPABASE_DB", "postgres"),
-        user=os.environ.get("SUPABASE_USER", "postgres.ncuofpvbaglwbdqnpman"),
-        password=os.environ.get("SUPABASE_PASSWORD", "W@elPreview2026"),
+        host="aws-1-eu-north-1.pooler.supabase.com",
+        port="6543",
+        database="postgres",
+        user="postgres.ncuofpvbaglwbdqnpman",
+        password="W@elPreview2026",
         sslmode="require",
         connect_timeout=30
     )
-
-
 
 def run_query(query, params=None):
     """تنفيذ استعلام والعودة كـ DataFrame"""
